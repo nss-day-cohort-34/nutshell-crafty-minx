34c0200c2ce288c67cbb21d40bb322092e0e09c4
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-08-19 15:30:31 -0500

    fixed merge issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,71 +4,76 @@ import renderRegistration from "./registration/domRegistration";
 import renderToArticles from "./articles/domArticles";
 import factoryArticles from "./articles/factoryArticles";
 import articlesData from "./articles/dataArticles";
+import initRegistration from "./registration/mainRegistration";
+import initEvents from "./events/mainEvents";
 
 
 /*----------------------REGISTRATION----------------------*/
-// Show welcome message and log in form when page loads
-const html = welcomePageHTML.createWelcome()
-renderRegistration(html)
-// Get reference to registration page container
-const registrationContainer = document.querySelector("#registration")
-// Create a new user object
-const createNewUser = (username, password) => {
-    return {
-        username: username.value,
-        password: password.value
-    }
-}
+initRegistration()
 
-// Get reference to username and email input fields
-// const usernameInput = document.querySelector("#username")
-// const passwordInput = document.querySelector("#password")
-// const inputsArray = [usernameInput, emailInput]
-// TABLED FOR LATER: Check if user filled out both input fields when they try to create account
-// const checkEmptyFields = () => {
-//     let validated
-//     for (let i = 0; i < inputsArray.length; i++) {
-//         const input = inputsArray[i]
-//         if (input.value === "") {
-//             validated = false
-//             alert("Please fill out both fields")
-//             break
-//         } else {
-//             validated = true
-//         }
-//         return validated
+// // Show welcome message and log in form when page loads
+// const html = welcomePageHTML.createWelcome()
+// renderRegistration(html)
+// // Get reference to registration page container
+// const registrationContainer = document.querySelector("#registration")
+// // Create a new user object
+// const createNewUser = (username, password) => {
+//     return {
+//         username: username.value,
+//         password: password.value
 //     }
 // }
 
-// Event listener on results container
-registrationContainer.addEventListener("click", () => {
-    if (event.target.id.startsWith("noAccount")) {
-        registrationContainer.innerHTML = ""
-        const newAccountHTML = welcomePageHTML.createAccount()
-        renderRegistration(newAccountHTML)
-    } else if (event.target.id.startsWith("logIn")) {
-        registrationContainer.innerHTML = ""
-    } else if (event.target.id.startsWith("saveNewAccount")) {
-        // const resultOfValidation = checkEmptyFields()
-        // if (resultOfValidation) {
-        const usernameInput = document.querySelector("#username")
-        const passwordInput = document.querySelector("#password")
-        const newUser = createNewUser(usernameInput, passwordInput)
-        API.saveNewUser(newUser)
-            .then(newUser => {
-                sessionStorage.setItem("activeUser", newUser.id)
-                let sessionUser = sessionStorage.getItem("activeUser")
-                console.log(sessionUser)
-            })
-        // }
-    } else {
-        event.stopPropagation()
-    }
-})
+// // Get reference to username and email input fields
+// // const usernameInput = document.querySelector("#username")
+// // const passwordInput = document.querySelector("#password")
+// // const inputsArray = [usernameInput, emailInput]
+// // TABLED FOR LATER: Check if user filled out both input fields when they try to create account
+// // const checkEmptyFields = () => {
+// //     let validated
+// //     for (let i = 0; i < inputsArray.length; i++) {
+// //         const input = inputsArray[i]
+// //         if (input.value === "") {
+// //             validated = false
+// //             alert("Please fill out both fields")
+// //             break
+// //         } else {
+// //             validated = true
+// //         }
+// //         return validated
+// //     }
+// // }
+
+// // Event listener on results container
+// registrationContainer.addEventListener("click", () => {
+//     if (event.target.id.startsWith("noAccount")) {
+//         registrationContainer.innerHTML = ""
+//         const newAccountHTML = welcomePageHTML.createAccount()
+//         renderRegistration(newAccountHTML)
+//     } else if (event.target.id.startsWith("logIn")) {
+//         registrationContainer.innerHTML = ""
+//     } else if (event.target.id.startsWith("saveNewAccount")) {
+//         // const resultOfValidation = checkEmptyFields()
+//         // if (resultOfValidation) {
+//         const usernameInput = document.querySelector("#username")
+//         const passwordInput = document.querySelector("#password")
+//         const newUser = createNewUser(usernameInput, passwordInput)
+//         API.saveNewUser(newUser)
+//             .then(newUser => {
+//                 sessionStorage.setItem("activeUser", newUser.id)
+//                 let sessionUser = sessionStorage.getItem("activeUser")
+//                 console.log(sessionUser)
+//             })
+//         // }
+//     } else {
+//         event.stopPropagation()
+//     }
+// })
 /*----------------------END REGISTRATION----------------------*/
 /*----------------------TASKS----------------------*/
 /*----------------------END TASKS----------------------*/
 /*----------------------EVENTS----------------------*/
+initEvents()
 /*----------------------END EVENTS----------------------*/
 /*----------------------ARTICLES----------------------*/
 const welcomeConverted = factoryArticles.factoryArticlesWelcome()
@@ -119,14 +124,4 @@ articleContainer.addEventListener("click", event => {
 /*----------------------END MESSAGES----------------------*/
 /*----------------------FRIENDS (MAYBE)----------------------*/
 /*----------------------END FRIENDS----------------------*/
-import initRegistration from "./registration/mainRegistration";
-import initEvents from "./events/mainEvents";
 
-/*----------------------REGISTRATION----------------------*/
-initRegistration()
-/*----------------------TASKS----------------------*/
-/*----------------------EVENTS----------------------*/
-// initEvents()
-/*----------------------ARTICLES----------------------*/
-/*----------------------MESSAGES----------------------*/
-/*----------------------FRIENDS (MAYBE)----------------------*/
